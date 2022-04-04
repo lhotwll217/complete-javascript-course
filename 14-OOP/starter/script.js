602715239fe113d80a27358c58d5b486dc4e535c
lhotwll217 <lukehenryotwell@gmail.com>
2022-04-04 03:28:57 -0400

    Object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -156,3 +156,27 @@ Person.hey = function () {
 Person.hey();
 
 PersonCl.hey();
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+//Creates a new object using the Proto of the object passed to .create
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
